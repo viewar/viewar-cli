c6d11130f486a10ce20b1dbf093a2889cbb8df27
viewar_sf <dev@project23.org>
2019-11-25 20:28:41 +0100

    add some more tests

--- tests/commander.spec.js
+++ tests/commander.spec.js
@@ -21,10 +21,28 @@ function cli(args, cwd) {
 describe('[parameters]', () => {
   test('--version', async () => {
     let result = await cli(['--version'], '.');
+
     expect(/^\d+\.\d+\.\d+/i.test(result.stdout)).toBe(true);
   });
+
   test('--help', async () => {
     let result = await cli(['--help'], '.');
     expect(result.code).toBe(0);
   });
+});
+
+describe('[commands]', () => {
+  test('whoami', async () => {
+    let result = await cli(['whoami'], '.');
+    expect(result.code).toBe(0);
+
+    expect(typeof result.stdout).toBe('string');
+    expect(result.stdout.length).toBeGreaterThan(0);
+  });
+
+  // test('login', async () => {
+  //   // TODO: use test-user
+  //   let result = await cli(['whoami'], '.');
+  //   expect(result.code).toBe(0);
+  // });
 });
