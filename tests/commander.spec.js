f1f565486e4322b0c162efd6ef6b7c7d078bd37e
viewar_sf <dev@project23.org>
2020-03-12 12:48:33 +0100

    chore(tests): fix jest tests

--- tests/commander.spec.js
+++ tests/commander.spec.js
@@ -1,48 +1,49 @@
 let path = require('path');
-let exec = require('child_process').exec;
+const util = require('util');
+let exec = util.promisify(require('child_process').exec);
+let spawn = require('child_process').spawn;
 
-function cli(args, cwd) {
-  return new Promise(resolve => {
-    exec(
-      `node ${path.resolve('./dist/index.js')} ${args.join(' ')}`,
-      { cwd },
-      (error, stdout, stderr) => {
-        resolve({
-          code: error && error.code ? error.code : 0,
-          error,
-          stdout,
-          stderr,
-        });
-      }
-    );
-  });
+jest.setTimeout(30000);
+
+async function cli(args, cwd) {
+  const result = await exec(
+    `node ${path.resolve('./dist/index.js')} ${args.join(' ')}`,
+    { cwd }
+  );
+  // remove process.argv print
+  result.stdout = result.stdout.split(/\n/);
+  result.stdout.pop();
+  result.stdout = result.stdout.pop();
+
+  return result;
 }
 
 describe('[parameters]', () => {
   test('--version', async () => {
     let result = await cli(['--version'], '.');
 
+    expect(result.stderr).toBe('');
     expect(/^\d+\.\d+\.\d+/i.test(result.stdout)).toBe(true);
   });
 
   test('--help', async () => {
     let result = await cli(['--help'], '.');
-    expect(result.code).toBe(0);
+    expect(result.stderr).toBe('');
   });
 });
 
 describe('[commands]', () => {
   test('whoami', async () => {
     let result = await cli(['whoami'], '.');
-    expect(result.code).toBe(0);
 
+    expect(result.stderr).toBe('');
     expect(typeof result.stdout).toBe('string');
     expect(result.stdout.length).toBeGreaterThan(0);
   });
 
+  // TODO: login as test-user and check whoami
   // test('login', async () => {
-  //   // TODO: use test-user
   //   let result = await cli(['whoami'], '.');
-  //   expect(result.code).toBe(0);
+  //   expect(result.stderr).toBe('');
   // });
 });
