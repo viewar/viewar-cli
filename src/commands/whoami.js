51a11354b78104cf14dc7662555ddccb55e22e0d
Karl Hofer <kh@viewar.com>
2020-09-10 16:53:11 +0200

    Show message with "whoami" if no user logged in yet.

--- src/commands/whoami.js
+++ src/commands/whoami.js
@@ -1,11 +1,19 @@
 import { cliConfigPath } from '../common/constants';
 import { readJson } from '../common/json';
 import logger from '../logger/logger';
+const emojic = require('emojic');
 
 export default async () => {
   logger.setInfo('whoami');
 
   const data = await readJson(cliConfigPath);
+
+  if (!Object.values(data.users).length) {
+    console.log(
+      `${emojic.bustInSilhouette}  Not logged in yet. Please use the 'login' command first.`
+    );
+  }
+
   console.log(
     Object.values(data.users)
       .map(user => `${user.name} <${user.email}>`)
